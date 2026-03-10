f06b8bb2de805a0fae10d5a04ad9a7b6adba4ff7
Gareth <gareth848@hotmail.com>
2026-03-10 16:23:11 +0000

    EDIT: say hello to Victoria, bump major version number

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,9 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Chris"));
         System.out.println(sayHelloToSomeone("Gareth"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
